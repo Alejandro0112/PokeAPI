9e7f3e832d6ae8795e034debe82d83fd933d7f22
Alejandro0112 <edwinal435@gmail.com>
2023-10-17 12:48:21 -0600

    Redirect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route, Routes } from 'react-router-dom';
+import {BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Pokemon from './Components/pokemon.js';
 import Machines from './Components/Extras/machines';
+import { useNavigate } from 'react-router-dom';
 class App extends React.Component {
  
   render(){
     return (
       <BrowserRouter>
         <Routes>
+        
+            <Route exact path='/' element={<Navigate to="/Home"/>}></Route>
             <Route path='/Home' element={<Pokemon/>}></Route>
             <Route path='/Machines' element={<Machines/>}></Route>
         </Routes>
